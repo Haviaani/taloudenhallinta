90ad13155f4424f56cbf68ed4b92194c6f77fa00
heiwi <heiwis82@gmail.com>
2024-12-11 00:33:42 +0200

    Lisää Header-komponentin

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.scss'
+
+function Header() {
+
+    return (
+        <div className={styles.header}>
+            Taloudenhallinta
+        </div>
+    )
+}
+
+export default Header
